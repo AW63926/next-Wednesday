c932b1ff1b0ceca883c42fc93721b94344711b1c
Adam Wood <aw63926@gmail.com>
2019-10-12 22:41:57 -0400

    first commit from repo

---
+++ src/main/java/com/nextwednesday/next/Wednesday/NextWednesdayApplication.java
@@ -0,0 +1,13 @@
+package com.nextwednesday.next.Wednesday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextWednesdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextWednesdayApplication.class, args);
+	}
+
+}
